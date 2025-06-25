3e2cf8a971ab4b48642082d6578fd1978b6bb599
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-06-25 16:36:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,18 +2,19 @@
 async function fetchRecipesData() {
   const response = await fetch("data/recipes.json");
   return await response.json();
+  console.log("Data fetched successfully");
 }
 
-function recipeData(data) {
-  const { id, image, name, ingredients, time, description, appliance, ustensils } = data;
-  console.log("Recipe Data:", data);
-  const picture = "recipes.js";
-}
+// function recipeData(data) {
+//   const { id, image, name, ingredients, time, description, appliance, ustensils } = data;
+//   console.log("Recipe Data:", data);
+//   const picture = "recipes.js";
+// }
 
-function getRecipeCard(data) {
-  const recipeDescription = document.createElement("p");
-  recipeDescription.textContent = data.description || "Recipe Description";
-  const recipeTitleAndDescription = document.getElementById("recipe-title-and-description");
-  recipeTitleAndDescription.appendChild(recipeDescription);
-  return recipeDescription;
-}
+// function getRecipeCard(data) {
+//   const recipeDescription = document.createElement("p");
+//   recipeDescription.textContent = data.description || "Recipe Description";
+//   const recipeTitleAndDescription = document.getElementById("recipe-title-and-description");
+//   recipeTitleAndDescription.appendChild(recipeDescription);
+//   return recipeDescription;
+// }

219ef9cf13d62154a48fe0e11cdea0f5e4ddb4db
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-06-25 15:45:00 +0200

    html corrected and css file added
    html corrected and css file added

--- main.js
+++ main.js
@@ -19,35 +19,6 @@ function recipeData(data) {
   const picture = "recipes.js";
 }
 
-function getRecipeCard(data) {
-  const {
-    id,
-    image,
-    name,
-    ingredients,
-    time,
-    description,
-    appliance,
-    ustensils,
-  } = data;
-  return `
-    <div class="recipe-card" id="${id}">
-      <img src="${image}" alt="${name}" />
-      <h3>${name}</h3>
-      <p>${description}</p>
-      <p>Time: ${time} min</p>
-      <p>Appliance: ${appliance}</p>
-      <p>Ustensils: ${ustensils.join(", ")}</p>
-      <ul>
-        ${ingredients
-          .map(
-            (ingredient) =>
-              `<li>${ingredient.ingredient}: ${ingredient.quantity || ""} ${
-                ingredient.unit || ""
-              }</li>`
-          )
-          .join("")}
-      </ul>
-    </div>`;
-  console.log("Recipe Card:", getRecipeCard(data));
-}
+// function getRecipeCard(data) {
+
+// }

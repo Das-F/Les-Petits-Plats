c5654a704780adc8fc6b70a3957ace8de1ba4460
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-07-01 20:21:59 +0200

    Recipe card construction

--- scripts/recipecard.js
+++ scripts/recipecard.js
@@ -1,21 +1,52 @@
-import { fetchRecipesData } from "./fetchRecipesData.js";
+import { fetchRecipesData } from "./fetch.js";
 
 export async function createRecipeCard() {
   const data = await fetchRecipesData();
 
   const recipe = data[0];
 
-  const img = document.getElementById("recipe-image");
-  if (img) {
-    img.src = recipe.image;
-    console.log("Recipe Image:", img.src);
-  }
-
-  const overlay = document.getElementById("recipe-image-overlay");
-  if (overlay) {
-    overlay.textContent = recipe.time + " minutes";
-    console.log("Recipe Time:", overlay.textContent);
-  }
+  const recipeCard = document.createElement("div");
+  recipeCard.className = "recipe-card flex flex-wrap justify-evenly w-sm h-300px gap-8 px-10 py-4 bg-white";
+  const recipeList = document.getElementById("recipe-list");
+  recipeList.appendChild(recipeCard);
 }
-
 createRecipeCard();
+
+export async function createCardImgContainer() {
+  const data = await fetchRecipesData();
+
+  const recipe = data[0];
+
+  const cardImgContainer = document.createElement("div");
+  cardImgContainer.className = "flex w-sm h-3xs rounded-lg bg-blue-500 shadow-md";
+  const recipeImgContainer = document.getElementById("recipe-img-container");
+  recipeImgContainer.appendChild(cardImgContainer);
+}
+createCardImgContainer();
+
+export async function createCardImg() {
+  const data = await fetchRecipesData();
+  const recipe = data[0];
+
+  const createCardImg = document.createElement("div");
+  createCardImg.className = "recipe-card flex flex-wrap justify-evenly w-sm h-300px gap-10 px-10 py-4";
+
+  const recipeCardImg = document.createElement("img");
+  recipeCardImg.src = recipe.image;
+  recipeCardImg.alt = recipe.name;
+
+  createCardImg.appendChild(recipeCardImg);
+}
+createCardImg();
+
+export async function createCardContent() {
+  const data = await fetchRecipesData();
+
+  const recipe = data[0];
+
+  const cardContent = document.createElement("div");
+  cardContent.className = "flex flex-col font-bold text-lg justify-start items-center px-2";
+  const recipeCard = document.getElementById("recipe-card");
+  recipeCard.appendChild(cardContent);
+}
+createCardContent();

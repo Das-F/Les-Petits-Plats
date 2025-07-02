98ff745ebd8f02b1d6cb185acd6bd04019ff8436
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-07-02 10:43:10 +0200

    aria-label, titles changed and recipeCard corrected
    aria-label deleted, titles changed and recipeCard corrected

--- scripts/recipecard.js
+++ scripts/recipecard.js
@@ -6,47 +6,8 @@ export async function createRecipeCard() {
   const recipe = data[0];
 
   const recipeCard = document.createElement("div");
-  recipeCard.className = "recipe-card flex flex-wrap justify-evenly w-sm h-300px gap-8 px-10 py-4 bg-white";
+  recipeCard.className = "recipe-card flex w-[380px] h-[600px] bg-white rounded-xl shadow-md overflow-hidden flex flex-col";
   const recipeList = document.getElementById("recipe-list");
   recipeList.appendChild(recipeCard);
 }
 createRecipeCard();
-
-export async function createCardImgContainer() {
-  const data = await fetchRecipesData();
-
-  const recipe = data[0];
-
-  const cardImgContainer = document.createElement("div");
-  cardImgContainer.className = "flex w-sm h-3xs rounded-lg bg-blue-500 shadow-md";
-  const recipeImgContainer = document.getElementById("recipe-img-container");
-  recipeImgContainer.appendChild(cardImgContainer);
-}
-createCardImgContainer();
-
-export async function createCardImg() {
-  const data = await fetchRecipesData();
-  const recipe = data[0];
-
-  const createCardImg = document.createElement("div");
-  createCardImg.className = "recipe-card flex flex-wrap justify-evenly w-sm h-300px gap-10 px-10 py-4";
-
-  const recipeCardImg = document.createElement("img");
-  recipeCardImg.src = recipe.image;
-  recipeCardImg.alt = recipe.name;
-
-  createCardImg.appendChild(recipeCardImg);
-}
-createCardImg();
-
-export async function createCardContent() {
-  const data = await fetchRecipesData();
-
-  const recipe = data[0];
-
-  const cardContent = document.createElement("div");
-  cardContent.className = "flex flex-col font-bold text-lg justify-start items-center px-2";
-  const recipeCard = document.getElementById("recipe-card");
-  recipeCard.appendChild(cardContent);
-}
-createCardContent();

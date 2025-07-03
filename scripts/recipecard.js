8ce664490a2e905ae955baa58b07131e2cdb9d4d
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-07-03 14:42:59 +0200

    div id in html deleted and JS functions added

--- scripts/recipecard.js
+++ scripts/recipecard.js
@@ -1,47 +1,48 @@
 import { fetchRecipesData } from "./fetch.js";
 
 export async function createRecipeCard() {
-  const data = await fetchRecipesData();
-  const recipe = data[0];
-
   const recipeCard = document.createElement("div");
   recipeCard.className = "relative flex w-[380px] h-[700px] bg-white rounded-xl shadow-md overflow-hidden flex flex-col";
+  recipeCard.id = "recipe-card";
   const recipeList = document.getElementById("recipe-list");
   recipeList.appendChild(recipeCard);
 }
 
-export async function createCardImageContainer() {
-  const data = await fetchRecipesData();
-  const recipe = data[0];
-
+const container = "container";
+export async function createCardImageContainer(container) {
   const imgContainer = document.createElement("div");
-  imgContainer.className = "flex w-full h-[240px] rounded-lg bg-white shadow-md";
-  const recipeCard = document.getElementById("recipe-card");
+  imgContainer.id = "recipe-img-container";
+  imgContainer.className = "flex w-full h-[240px] rounded-lg bg-blue-500 shadow-md";
+  imgContainer.textContent = "Image Container";
+  recipeCard.id = "recipe-card";
   imgContainer.appendChild(recipeCard);
-  return imgContainer;
 }
-
-export async function createCardImage() {
-  const data = await fetchRecipesData();
-  const recipe = data[0];
-
+const img = { src: "images/background.jpg" };
+export async function createCardImage(img) {
   const recipeImage = document.createElement("img");
   recipeImage.className = "object-cover w-full h-full";
   recipeImage.src = recipe.image;
   const imgContainer = document.getElementById("recipe-img-container");
   imgContainer.appendChild(recipeImage);
-  return recipeImage;
 }
-
-export async function createCardImageOverlay() {
-  const data = await fetchRecipesData();
-  const recipe = data[0];
-
+const timeOverlay = "60min";
+export async function createCardImageOverlay(timeOverlay) {
   const overlay = document.createElement("div");
   overlay.className = "absolute top-4 right-4 bg-yellow-400 text-black text-sm font-medium px-3 py-1 rounded-full shadow-md";
   overlay.textContent = `${recipe.time}mn`;
   imgContainer.appendChild(overlay);
-  return overlay;
+}
+const title = "Tarte aux mirabelles";
+function createTitleDescriptionRecipe(title) {
+  const createTitleDescriptionRecipe = document.createElement("div");
+  createTitleDescriptionRecipe.id = "recipe-title-and-description";
+  createTitleDescriptionRecipe.textContent = title;
+  const recipeCard = document.getElementById("recipe-card");
+  recipeCard.appendChild(createTitleDescriptionRecipe);
 }
 
 createRecipeCard();
+createCardImageContainer(container);
+createCardImage(img);
+createCardImageOverlay(timeOverlay);
+createTitleDescriptionRecipe(title);

c37a3a7c7040e695f128e995ccbfd9d487bc4244
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-07-11 19:02:37 +0200

    algorithm 1 corrected

--- scripts/algorithm-1.js
+++ scripts/algorithm-1.js
@@ -3,25 +3,20 @@ const searchResults = document.createElement("div");
 searchResults.className = "search-results";
 searchInput.parentNode.appendChild(searchResults);
 
-let dataArray = [];
+fetch("recipes.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const recipe = [...data];
+    console.log(recipe);
+  });
 
-async function getRecipes() {
-  const response = await fetch("recipes.json");
-  const { recipes } = await response.json();
-  dataArray = recipes;
-  return recipes;
-}
-getRecipes().then((data) => {
-  console.log(data);
-});
-
-function createSearchAlgorithmOne() {
+function createSearchAlgorithm() {
   searchInput.addEventListener("input", (e) => {
     const value = e.target.value;
     console.log(value);
   });
 }
-createSearchAlgorithmOne();
+createSearchAlgorithm();
 
 // Prépa des données pour l'algorithme de recherche
 // Configuration des options de recherche

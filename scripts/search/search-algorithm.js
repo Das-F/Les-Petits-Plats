142d743ec761d0c07a9804cc30643833614ca9db
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-07-29 11:27:10 +0200

    refactored code to fix search bug
    code refactored to fix problem when we search in input, then tag, then remove the input text and then tag.

--- scripts/search/search-algorithm.js
+++ scripts/search/search-algorithm.js
@@ -2,47 +2,19 @@
 // It listens for input in the search field and filters recipes based on the input value.
 
 // Version 1
-export function createSearchAlgorithm(recipes, displayRecipes) {
+export function createSearchAlgorithm(onSearchChange) {
   const searchInput = document.getElementById("search-input");
-  const searchResults = document.createElement("div");
-  searchResults.className = "search-results text-white mt-4";
-  const mainHead = document.querySelector(".main-head");
-  mainHead.appendChild(searchResults);
-  const recipeList = document.querySelector(".recipe-list");
-  const MIN_LENGTH_SEARCH = 3;
 
   searchInput.addEventListener("input", (e) => {
     const value = e.target.value.trim().toLowerCase();
-    recipeList.innerHTML = "";
-
-    if (value.length === 0) {
-      searchResults.innerHTML = "";
-      displayRecipes(recipes, false);
-    } else if (value.length < MIN_LENGTH_SEARCH) {
-      searchResults.innerHTML = `Merci de saisir au moins ${MIN_LENGTH_SEARCH} caractères.`;
-      displayRecipes(recipes, false);
-    } else {
-      searchResults.innerHTML = "";
-      const results = filterRecipesBySearch(recipes, value);
-
-      // Tri alphabétique
-      results.sort((a, b) => a.name.localeCompare(b.name));
-
-      if (results.length === 0) {
-        searchResults.innerText = `Aucune recette ne contient "${value}" vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
-        displayRecipes([], true);
-      } else {
-        displayRecipes(results, true);
-      }
-    }
+    onSearchChange(value);
   });
 }
 
-function filterRecipesBySearch(recipes, searchValue) {
+export function filterRecipesBySearch(recipes, searchValue) {
   const value = searchValue.trim().toLowerCase();
-  if (value.length < 3) return recipes;
 
-  return recipes.filter((recipe) => {
+  const results = recipes.filter((recipe) => {
     const name = recipe.name ? recipe.name.toLowerCase() : "";
     const description = recipe.description ? recipe.description.toLowerCase() : "";
 
@@ -53,4 +25,7 @@ function filterRecipesBySearch(recipes, searchValue) {
     }
     return false;
   });
+
+  results.sort((a, b) => a.name.localeCompare(b.name));
+  return results;
 }
